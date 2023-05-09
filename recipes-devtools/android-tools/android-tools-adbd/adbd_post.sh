d7555103574d7989af37f033b94f9d33196a9b50
Jiacai.Liu <jiacai.liu@amlogic.com>
2023-05-09 15:37:46 +0800

    adb: CB use node check instead of sleep [1/1]
    PD#SWPL-123049
    
    Problem:
    adb_post.sh need time to wait, the time is set 3s,
    it not suit for all platform
    
    Solution:
    use /dev/usb-ffs/adb/ep1 check to instead
    
    Verify:
    local s1a
    
    Signed-off-by: Jiacai.Liu <jiacai.liu@amlogic.com>
    Change-Id: I64fb09124d1ebcc7a91bf424ce753ca6cf6abb14

--- recipes-devtools/android-tools/android-tools-adbd/adbd_post.sh
+++ recipes-devtools/android-tools/android-tools-adbd/adbd_post.sh
@@ -5,7 +5,12 @@ usb_net_ipconfig()
     ifconfig usb0 192.168.5.1 up
 }
 
-sleep 3
+#the ep1&ep2 is created by adbd, adbd start need time, here use node check instead of sleep
+# if adbd start complete, ep1&ep2 will create
+i=1
+while [ $i -le 20 ] && [ ! -f /dev/usb-ffs/adb/ep1 ]
+do sleep 0.5;i=`expr $i + 1`;
+done
 
 if [ -f /etc/adb_udc_file ]; then
     echo $(cat /etc/adb_udc_file) > /sys/kernel/config/usb_gadget/amlogic/UDC
