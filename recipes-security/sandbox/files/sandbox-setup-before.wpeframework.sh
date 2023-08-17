7b68c54a679267934ee7d2dc2e7f3a478880de4c
guangjun.zhu <guangjun.zhu@amlogic.com>
2023-08-17 14:30:32 +0800

    sandbox: CB2 remove code that creates directories for avpk. [1/1]
    PD#TV-89684
    
    Problem:
    Customer projects may use other directories, so the directory created
    here will be invalid.
    
    Solution:
    Remove code that creates directories for avpk.
    It is up to avpk to create a directory that meets the requirements.
    
    Verify:
    t5w
    
    Signed-off-by: guangjun.zhu <guangjun.zhu@amlogic.com>
    Change-Id: I229f24d6403ad368836a4f4bf089001719ad2949

--- recipes-security/sandbox/files/sandbox-setup-before.wpeframework.sh
+++ recipes-security/sandbox/files/sandbox-setup-before.wpeframework.sh
@@ -10,14 +10,6 @@ for D in /data/persistent; do
   fi
 done
 
-for D in /tmp/amazonPrime /data/amazonPrime; do
-  mkdir -p ${D}
-  if [ -d "${D}" ];then
-    chgrp $GROUP_SESSION "${D}"
-    chmod g+rw "${D}"
-  fi
-done
-
 echo 0 > /sys/module/drm/parameters/vblankoffdelay
 echo 1 > /sys/module/aml_drm/parameters/video_axis_zoom
 
